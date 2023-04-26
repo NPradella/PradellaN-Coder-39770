2990a5dc733668922300f74caf6ad5b51df5c4b0
pradella_nicolas <nicolaspradella98@gmail.com>
2023-04-25 21:53:29 -0300

    desafio 2

---
+++ data/data.js
@@ -0,0 +1,92 @@
+[
+  {
+    "title": "cama",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 1
+  },
+  {
+    "title": "mesa",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 2
+  },
+  {
+    "title": "puerta",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 3
+  },
+  {
+    "title": "casco",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 4
+  },
+  {
+    "title": "remera",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 5
+  },
+  {
+    "title": "campera",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 6
+  },
+  {
+    "title": "pantalon",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 7
+  },
+  {
+    "title": "gorro",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 8
+  },
+  {
+    "title": "pelota",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 9
+  },
+  {
+    "title": "velador",
+    "description": "para dormir",
+    "price": 5,
+    "thumbnail": "imgsource",
+    "code": 1,
+    "stock": 10,
+    "id": 11
+  }
+]
